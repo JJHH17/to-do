96b581b422320a2e73e601e4e685f4ac68c1c9ce
James Hatfield <james.hatfield17@icloud.com>
2025-03-07 11:37:33 +0000

    Add class to build new project

---
+++ src/projectBuilder.js
@@ -0,0 +1,17 @@
+// Responsible for building the project Objects
+
+export class Project {
+    constructor(title, description) {
+        this.title = title;
+        this.description = description;
+    }
+}
+
+// User is able to create a project here
+function newProject() {
+    let projectName = prompt("Please enter a project name: ");
+    let projectDescription = prompt("Please enter a project description");
+
+    let newProject = new Project(projectName, projectDescription);
+    return newProject;
+}
