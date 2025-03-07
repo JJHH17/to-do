caf7d386ea2dad226c0e0ef64fb9cc962617397e
James Hatfield <james.hatfield17@icloud.com>
2025-03-07 20:45:33 +0000

    Refactor Project class, add to do method

--- src/projectBuilder.js
+++ src/projectBuilder.js
@@ -5,6 +5,12 @@ export class Project {
     constructor(title, description) {
         this.title = title;
         this.description = description;
+        this.toDos = []; // Stores an array of to do children objects
+    }
+    
+    addToDoItem(item, notes, dueDate, priority) {
+        const newToDo = newToDoItem(item, notes, dueDate, priority);
+        this.toDos.push(newToDo) // Pushes into array when created
     }
 }
 
