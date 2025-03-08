16d8ff34dd8b58e6acfbc88997dc59c4dbf0594c
James Hatfield <james.hatfield17@icloud.com>
2025-03-08 20:43:28 +0000

    Refactor started to simplify project

--- src/index.js
+++ src/index.js
@@ -1,12 +1,1 @@
 import "./styles.css";
-import { Project } from "./projectBuilder";
-import { printToSidebar, buttonSelector } from "./sidebar";
-import { createProjectModal } from "./buildProjectModal";
-import { addToDoButton } from "./createToDoModal";
-import { toDoFormItems, toDoButton } from "./createToDoModal";
-
-// Button press creates modal
-buttonSelector().addEventListener("click", () => {
-    createProjectModal();
-})
-
